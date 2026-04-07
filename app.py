6edfc57378e36d9444c4e3819ba4df77234c3489
Aurélien Roig <aurelien.roig.pro@gmail.com>
2026-04-07 23:50:01 +0200

    Ceci est le 3ième essai

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import pickle
 import pandas as pd
 
-# Bonjour à tous, ici paris
+# Bonjour à tous, ici paris vers londre 
 app = Flask(__name__)
 model = pickle.load(open("catboost_model-2.pkl", "rb"))
 
